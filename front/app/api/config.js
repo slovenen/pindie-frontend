3e90682e0958dd82d3cbe43b2e4df68c94140c2e
slovenen <sidorov15111998@gmail.com>
2024-05-31 16:13:31 +0300

    deploy front

--- front/app/api/config.js
+++ front/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-zhuravlevilya.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://slovenen-pindie-backend.nomorepartiesco.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
